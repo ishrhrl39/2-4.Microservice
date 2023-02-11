03f7a3410638d3015a8641c5908ffd96b2f54561
이현섭 <@DESKTOP-4VPH1H9>
2023-02-11 18:31:39 +0900

    초안 업로드

--- item-service/src/main/java/org/msa/item/controller/ItemController.java
+++ item-service/src/main/java/org/msa/item/controller/ItemController.java
@@ -2,24 +2,30 @@ package org.msa.item.controller;
 
 import org.msa.item.dto.ItemDTO;
 import org.msa.item.dto.ResponseDTO;
+import org.msa.item.service.ItemService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value="v1/item")
 @Slf4j
+@RequiredArgsConstructor
 public class ItemController {
 	
+	private final ItemService itemService;
+	
 	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public ResponseEntity<ResponseDTO> add(@RequestBody ItemDTO itemDTO){
+	public ResponseEntity<ResponseDTO> add(@Valid @RequestBody ItemDTO itemDTO){
 		ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
 		
+		itemService.insertItem(itemDTO);
 		log.debug("request add item id = {}", itemDTO.getId());
 		
 		responseBuilder.code("200").message("success");
